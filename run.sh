43779326c97256c59fb7cf9a84475a3a3c801f59
Yuan Yuan <940545897@qq.com>
2023-06-24 14:00:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
-python app.py --dataset Earthquake --mode train --timesteps 200 --samplingsteps 200 --batch_size 64 --cuda_id 0 --total_epochs 2000
+python app.py --dataset Earthquake --mode train --timesteps 500 --samplingsteps 500 --batch_size 64 --cuda_id 0 --total_epochs 2000
 
-python app.py --dataset COVID19 --mode train --timesteps 200 --samplingsteps 200 --batch_size 64 --cuda_id 0 --total_epochs 2000
+python app.py --dataset COVID19 --mode train --timesteps 500 --samplingsteps 500 --batch_size 64 --cuda_id 0 --total_epochs 2000
 
-python app.py --dataset Pinwheel --mode train --timesteps 200 --samplingsteps 200 --batch_size 256 --cuda_id 0 --total_epochs 2000 
+python app.py --dataset Citybikes --mode train --timesteps 500 --samplingsteps 500 --batch_size 128 --cuda_id 0 --total_epochs 2000 
 
-python app.py --dataset HawkesGMM --mode train --timesteps 200 --samplingsteps 200 --batch_size 256 --cuda_id 0 --total_epochs 2000
-
-python app.py --dataset Mobility --mode train --timesteps 200 --samplingsteps 200 --batch_size 128 --cuda_id 0 --total_epochs 2000 
-
-python app.py --dataset Citybikes --mode train --timesteps 200 --samplingsteps 200 --batch_size 128 --cuda_id 0 --total_epochs 2000 
-
-python app.py --dataset Independent --mode train --timesteps 200 --samplingsteps 200 --batch_size 128 --cuda_id 0 --total_epochs 2000 
+python app.py --dataset Independent --mode train --timesteps 500 --samplingsteps 500 --batch_size 128 --cuda_id 0 --total_epochs 2000 
